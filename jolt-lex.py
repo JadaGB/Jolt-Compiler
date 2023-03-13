c52b3d6d99cfa7177590304434856bd001392a32
Garciian <garcianamairs@gmail.com>
2023-03-13 02:40:38 -0500

    Tokens

--- jolt-lex.py
+++ jolt-lex.py
@@ -9,33 +9,109 @@ import ply.lex as lex
 
 # List of token names.   This is always required
 tokens = (
-   'NUMBER',
+    #Literals
+   'NUMBA',
+   'DECI',
+   'WUD',
+   'LETTA',
+   'WHICHEVA',
+
+    #Arithmetic Operators
    'PLUS',
    'MINUS',
    'TIMES',
    'DIVIDE',
-   'LPAREN',
-   'RPAREN',
-   'CONSTANTS',
+   'MODULUS',
+   'INCREMENT',
+   'DECREMENT',
+
+    #Logical Operators
+   'AND',
+   'OR',
+   'NOT',
+
+   #Comparative/Conditional Operators
+   'EQUAL',
+   'NOT_EQUAL',
+   'GREATER_THAN',
+   'LESS_THAN',
+   'GREATER_EQUAL',
+   'LESS_EQUAL',
+
+   #Symbols
+   'ASSIGNMENT',
+   'END_LINE',
+   'COMMENT',
+   'COLON',
+   'OPENBRACE',
+   'CLOSEBRACE',
+   'DOUBLE_LESS',
+   'DOUBLE_GREATER',
 )
 
-# Regular expression rules for simple tokens
+#Regular Expressions for Tokens - Simple
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
 t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-#t_NUMBER  = r'([0-9]+)' 
+t_MODULUS  = r'%'
+t_INCREMENT  = r'\+\+'
+t_DECREMENT  = r'\--'
 
+t_AND  = r'@'
+t_OR  = r'\|'
+t_NOT  = r'\?'
 
+t_EQUAL    = r'\=='
+t_NOT_EQUAL   = r'\?\='
+t_GREATER_THAN  = r'\>'
+t_LESS_THAN  = r'<'
+t_GREATER_EQUAL  = r'\>='
+t_LESS_EQUAL  = r'\<='
 
-# # A regular expression rule with some action code
-def t_NUMBER(t):
+t_ASSIGNMENT    = r'\='
+t_END_LINE   = r'\.'
+t_COLON  = r':'
+#t_COMMENT  = r'~.~*'  #'~.*~'  r'~.*~'
+t_DOUBLE_LESS  = r'\<<'
+t_DOUBLE_GREATER  = r'\>>'
+t_OPENBRACE  = r'\('
+t_CLOSEBRACE  = r'\)'
+
+#Regular Expressions for Tokens - Functions
+def t_DECI(t):
+    r'^\d+\.\d+$'
+    t.value = float(t.value)
+    return t
+
+def t_NUMBA(t):
     r'([0-9]+)' 
     t.value = int(t.value)  
     return t
 
+def t_LETTA(t):
+    r'"([a-zA-Z]?)"'
+    t.value = t.value
+    return t
+
+def t_WUD(t):
+    #Starts & ends with a double quote
+    # [^"\\] matches any character that is not a double quote or a backslash.
+    # | is the alternation operator, which means "or".
+    # \\. matches an escaped character (e.g., \" or \\).
+    r'"([^"\\]|\\.)*"'
+    t.value = t.value
+    return t
+
+def t_WHICHEVA(t):
+    r'True|False'
+    t.value = bool(t.value)
+    return t
+
+def t_COMMENT(t):
+    r'~.*~'
+    t.value = t.value
+    return t
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -47,16 +123,14 @@ t_ignore  = ' \t'
 
 # Error handling rule
 def t_error(t):
-      
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
 # Build the lexer
 lexer = lex.lex()
 
-data = '''
-3 + 4 * 10 + -20 *2
-'''
+data = '~as A $$$ GIRL~'
+#'''3.1'''
 #check for 3 + 4 * 10 + - 20
 #epr + term
 #term + term
@@ -74,6 +148,7 @@ while True:
     tok = lexer.token()
     if not tok: 
         break      # No more input
-    print(tok)
+    #print(tok) #-prints token type, value, position
+    print(tok.type,':', tok.value)
 
 
