f5be00b8eb2609a1c27e6ba6d73dc30991ac2c21
Garcian Mairs <mairsgarcian@gmail.com>
2023-03-15 04:49:52 -0500

    Added yacc file

---
+++ jolt_yacc.py
@@ -0,0 +1,55 @@
+import ply.yacc as yacc
+
+# Get the token map from the lexer. 
+from jolt_lex import tokens
+
+def p_literal(p):
+    '''
+    literal : NUMBA
+              | DECI
+              | LETTA
+              | WUD
+    '''
+    p[0] = ('literal', p[1])
+
+def p_symbol(p):
+    '''
+    symbol :  ASSIGNMENT
+              | END_LINE
+              | COMMENT
+              | COLON
+              | OPENBRACE
+              | CLOSEBRACE
+              | DOUBLE_LESS
+              | DOUBLE_GREATER
+    '''
+    p[0] = ('symbol', p[1])
+
+# def p_expression_plus(p):
+#     'expression : expression PLUS factor'
+#     p[0] = p[1] + p[3]
+
+# def p_expression_factor(p):
+#     'expression : factor'
+#     p[0] = p[1]
+
+# def p_factor_num(p):
+#     'factor : NUMBA'
+#     p[0] = p[1]
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax Error: Input invalid")
+
+# Build the parser
+parser = yacc.yacc()
+
+print('...............................Welcome to Jolt Terminal................................')
+while True:
+   try:
+       s = input('Jolt >> ')
+   except EOFError:
+       break
+   if not s: continue
+   result = parser.parse(s)
+   print(result)
