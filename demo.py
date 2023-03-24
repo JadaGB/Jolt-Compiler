e1dadca1520d0d2e7af90c4ad45161c093d36f51
GarcianM <mairsgarcian@gmail.com>
2023-03-24 17:46:44 -0500

    Updated demo to include interface and highlighting

--- demo.py
+++ demo.py
@@ -1,33 +1,55 @@
 import sys
+from PyQt5.QtCore import QRegExp, Qt
+from PyQt5.QtGui import QColor, QTextCharFormat, QSyntaxHighlighter, QFont
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QAction, QFileDialog, QWidget,QVBoxLayout,QPushButton
 from jolt_yacc import *
+from jolt_lex import reserved
 
+class MyHighlighter(QSyntaxHighlighter):
+    def __init__(self, parent=None):
+        super(MyHighlighter, self).__init__(parent)
+        self.highlighting_rules = []
+        
+        keyword_format = QTextCharFormat()
+        keyword_format.setForeground(Qt.blue)
+        keyword_format.setFontWeight(QFont.Bold)
+        keywords = reserved #['if', 'else', 'for', 'while']
+        for word in keywords:
+            pattern = QRegExp("\\b" + word + "\\b")
+            self.highlighting_rules.append((pattern, keyword_format))
+        
+        literal_format = QTextCharFormat()
+        literal_format.setForeground(Qt.darkGreen)
+        pattern = QRegExp("\\b[0-9]+\\b")
+        self.highlighting_rules.append((pattern, literal_format))
+        
+        operator_format = QTextCharFormat()
+        operator_format.setForeground(Qt.red)
+        opertors =  ['+', '-', '*', '/', '%', '=', '==','<', '>','>=','<=', '?=', '?', '|', '@']
+        for op in opertors:
+            pattern = QRegExp("\\" + op)
+            self.highlighting_rules.append((pattern, operator_format))
+        
+        comment_format = QTextCharFormat()
+        comment_format.setForeground(Qt.gray)
+        pattern = QRegExp("~.*~")
+        self.highlighting_rules.append((pattern, comment_format))
+        # pattern = QRegExp("/\*.*\*/")
+        pattern.setMinimal(True)
+        # self.highlighting_rules.append((pattern, comment_format))
+
+    def highlightBlock(self, text):
+        for pattern, char_format in self.highlighting_rules:
+            expression = QRegExp(pattern)
+            index = expression.indexIn(text)
+            while index >= 0:
+                length = expression.matchedLength()
+                self.setFormat(index, length, char_format)
+                index = expression.indexIn(text, index + length)
+                
 class IDE(QMainWindow):
     def __init__(self):
         super().__init__()
-         # Create text edit widgets
-        # self.code_input = QTextEdit(self)
-        # self.code_input.setGeometry(0, 0, 400, 600)
-        # self.code_input.setPlainText('')
-
-        # self.output_display = QTextEdit(self)
-        # self.output_display.setGeometry(400, 0, 400, 600)
-        # self.output_display.setReadOnly(True)
-
-        # # Create main widget and layout
-        # main_widget = QWidget(self)
-        # self.setCentralWidget(main_widget)
-        # layout = QVBoxLayout(main_widget)
-
-        # # Create text editor for code
-        # code_editor = QTextEdit()
-        # layout.addWidget(code_editor)
-
-        # # Create text editor for output
-        # output_editor = QTextEdit()
-        # output_editor.setReadOnly(True)
-        # layout.addWidget(output_editor)
-
         self.initUI()
 
     def initUI(self):
@@ -77,8 +99,10 @@ class IDE(QMainWindow):
         self.button.clicked.connect(self.run)
         self.button.move(680, 3)
 
+        self.highlighter = MyHighlighter(self.textEdit)
+
         self.setGeometry(400, 100, 800, 600)
-        self.setWindowTitle('Python IDE')
+        self.setWindowTitle('Jolt IDE')
         self.show()
 
     def openFile(self):
@@ -98,18 +122,13 @@ class IDE(QMainWindow):
                 file.write(self.textEdit.toPlainText())
 
     def run(self):
-        content = self.textEdit.get("1.0","end-1c").splitlines()
-        # file = saveToFile()
+        content = self.textEdit.toPlainText().splitlines()
         output = parseInput(content)
         self.output_editor.clear()
-        # output_widget.delete("1.0", "end-1c")
-
-        for line in reversed(output):
-            self.output_editor.insert("1.0", line)
-            self.textEdit.setText(line)
-            # output_widget.insert("1.0", "\n")
-            # print(line)
-        return content
+        for line in output:
+            self.output_editor.insertPlainText(str(line))
+            self.output_editor.insertPlainText("\n")
+        # return content
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
